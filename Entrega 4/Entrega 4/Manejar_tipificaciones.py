b771751069c99d063297d6e4fd07f4c5a56845df
jigracia <jigracia@miuandes.cl>
2019-10-09 22:09:56 -0300

    mostrar las campaigns al comienzo

--- Entrega 4/Entrega 4/Manejar_tipificaciones.py
+++ Entrega 4/Entrega 4/Manejar_tipificaciones.py
@@ -1,10 +1,67 @@
+#Funcion para hacer preguntas binarias
+def YesOrNo(pregunta):
+    while True:
+        respuesta=input(pregunta).lower()
+        if respuesta == "y" or respuesta=="n":
+            if respuesta == "y":
+                return "y"
+            if respuesta=="n":
+                return "n"
+        else:
+            print("Ingrese Opcion valida")
+
+#Funcion para mostrar las campañas disponibles para el tennat ademas de poder seleecionar 1 de ellas
+def ShowCampaigns(Login):
+    ListCampaign=[]
+    cursor=connection.cursor()
+    sentencia="select c.id_campaign, c.fecha_inicio, c.fecha_termino,c.nombre from tennants_campaigns t join campaign c on t.id_campaign = c.id_campaign where t.id_tennant ="+str(Login)
+    cursor.execute(sentencia)
+    rows=cursor.fetchall()
+    if len(rows)!=0:
+        for a in rows:
+            print("Id_Campaign:",a[0])
+            print("Nombre campaña:",a[3])
+            print("Fecha inicio:",a[1])
+            print("Fecha termino:",a[2])
+            print("-------------------------------")
+            ListCampaign.append(a[0])
+    else:
+        print("No hay campañas para este tennant")
+    cursor.close()  
+    return ListCampaign
 
 
+def ChooseCampaign(Login):
+    ListCampaign=ShowCampaigns(Login)
+    if len(ListCampaign)==0:
+        return (True,-1)
+    else:
+        while True:
+            choice=input("Seleccione un ID_campaña\n")
+            try:
+                choice=int(choice)
+                if choice in ListCampaign:
+                    Showtipification(Login,choice)
+                    return (True,choice)
+                else:
+                    print("Ingrese opcion valida")
+            except:
+                print("Ingrese opcion Valida")
 
-
-
-
-
+def Showtipification(Login,choice):
+    cursor=connection.cursor()
+    sentencia="select t.id_pregunta, t.tipo_dato,t.pregunta_asociada from (tipificaciones_campaign tic join tipificacion t on tic.id_pregunta = t.id_pregunta) join tennants_campaigns tec on tec.id_campaign = tic.id_campaign where tic.id_campaign="+str(Login)
+    cursor.execute(sentencia)
+    rows=cursor.fetchall()
+    if len(rows)!=0:
+        for a in rows:
+            print("Id_Pregunta:",a[0])
+            print("Pregunta asociada:",a[2])
+            print("Tipo de respuesta:",a[1])
+            print("-------------------------------")
+    else:
+        print("No hay tipifiaciones para esta campaña")
+    cursor.close()  
 
 
 
@@ -37,6 +94,7 @@ def Exit():
 
  
 if __name__ == "__main__":  
+    Connect()
     login=0
-
+    ChooseCampaign(login)
     Exit()
